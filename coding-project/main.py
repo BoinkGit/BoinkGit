101ef7b5e7f61b15eec4703e8e14deec61113808
BoinkGit <rmelyle@gmail.com>
2025-01-04 03:19:36 -0500

    added variables (undebugged) made better draw function

--- coding-project/main.py
+++ coding-project/main.py
@@ -9,12 +9,14 @@ for word in longcode:
         code.append(letter)
 for i in range(len(code)):
     code[i] = int(code[i])
+    if not code[i] in range(8):
+        raise ValueError("Values out of range 0-7")
 
 variables = [0,0,0,0,0,0,0,0]
 colorList = [BLACK,WHITE, RED,GREEN,DARKBLUE, YELLOW,VIOLET,SKYBLUE]
 pointer = Vector2(0,0)
 
-rects = []
+rects = {}
 
 init_window(512,512,"Fantasy Console")
 
@@ -22,6 +24,7 @@ index = 0
 
 while not window_should_close():
     begin_drawing()
+    clear_background(BLACK)
     if index < len(code):
         num = code[index]
 
@@ -30,13 +33,21 @@ while not window_should_close():
             pointer.y = eval(f"0o{code[index+3]}{code[index+4]}")*8
             index += 4
 
-        if num == 1:
-            a = f"draw_rectangle_v(Vector2({pointer.x},{pointer.y}),Vector2(8,8),{colorList[code[index+1]]})"
-            rects.append(a)
+        elif num == 1:
+            rects[f"({pointer.x}, {pointer.y})"] = colorList[code[index+1]]
             index += 1
 
+        elif num == 2:
+            if (index+2) == 0:
+                variables[index+1] = index+3
+            if (index+2) == 1:
+                variables[index+1] = variables[index+3]
+            if (index+2) == 2:
+                variables[index+1] = index+3
+            index += 3
+
     for rect in rects:
-       exec(rect)
+       draw_rectangle(int(eval(rect)[0]),int(eval(rect)[1]),8,8,rects[rect])
     end_drawing()
     index += 1
 
